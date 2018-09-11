a574e96bcf3d2cc714d329cc3f202da88f178b58
Damianonymous <zetixpe32@gmail.com>
2018-09-11 11:02:09 +0200

    Update dailymotion.py

--- dailymotion.py
+++ dailymotion.py
@@ -1,12 +1,9 @@
-import logging
 import re
 
 from streamlink.exceptions import NoStreamsError
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import validate
+from streamlink.plugin.api import http, validate
 from streamlink.stream import HLSStream, HTTPStream
-
-log = logging.getLogger(__name__)
 
 COOKIES = {
     "family_filter": "off",
@@ -15,8 +12,21 @@ COOKIES = {
 STREAM_INFO_URL = "https://www.dailymotion.com/player/metadata/video/{0}"
 USER_INFO_URL = "https://api.dailymotion.com/user/{0}"
 
+_url_re = re.compile(r"""
+    http(s)?://(\w+\.)?
+    dailymotion.com
+    (?:
+        (/embed)?/(video|live)
+        /(?P<media_id>[^_?/]+)
+    |
+        /(?P<channel_name>[A-Za-z0-9-_]+)
+    )
+""", re.VERBOSE)
+
 _media_schema = validate.Schema(validate.any(
     {"error": {"title": validate.text}},
+    # "stream_chromecast_url": validate.url(),
+    # Chromecast URL is already available in qualities subdict
     {"qualities": validate.any({
         validate.text: validate.all([{
             "type": validate.text,
@@ -36,21 +46,16 @@ _live_id_schema = validate.Schema(
 
 
 class DailyMotion(Plugin):
-    pattern = r'''(?x)https?://(\w+\.)?
-        dailymotion.com
-        (?:
-            (/embed)?/(video|live)
-            /(?P<media_id>[^_?/]+)
-        |
-            /(?P<channel_name>[A-Za-z0-9-_]+)
-        )'''
+    @classmethod
+    def can_handle_url(cls, url):
+        return _url_re.match(url)
 
     def _get_streams_from_media(self, media_id):
-        res = self.session.http.get(STREAM_INFO_URL.format(media_id), cookies=COOKIES)
-        media = self.session.http.json(res, schema=_media_schema)
+        res = http.get(STREAM_INFO_URL.format(media_id), cookies=COOKIES)
+        media = http.json(res, schema=_media_schema)
 
         if media.get("error"):
-            log.error("Failed to get stream: {0}".format(media["error"]["title"]))
+            self.logger.error("Failed to get stream: {0}".format(media["error"]["title"]))
             return
 
         for quality, streams in media['qualities'].items():
@@ -67,25 +72,28 @@ class DailyMotion(Plugin):
                     yield resolution, HTTPStream(self.session, stream['url'])
 
     def get_live_id(self, username):
+        """Get the livestream videoid from a username.
+           https://developer.dailymotion.com/tools/apiexplorer#/user/videos/list
+        """
         params = {
             "flags": "live_onair"
         }
         api_user_videos = USER_INFO_URL.format(username) + "/videos"
         try:
-            res = self.session.http.get(api_user_videos.format(username),
-                                        params=params)
-        except Exception:
-            log.error("invalid username")
+            res = http.get(api_user_videos.format(username),
+                           params=params)
+        except Exception as e:
+            self.logger.error("invalid username")
             raise NoStreamsError(self.url)
 
-        data = self.session.http.json(res, schema=_live_id_schema)
+        data = http.json(res, schema=_live_id_schema)
         if data["total"] > 0:
             media_id = data["list"][0]["id"]
             return media_id
         return False
 
     def _get_streams(self):
-        match = self.pattern_re.match(self.url)
+        match = _url_re.match(self.url)
         media_id = match.group("media_id")
         username = match.group("channel_name")
 
@@ -93,9 +101,10 @@ class DailyMotion(Plugin):
             media_id = self.get_live_id(username)
 
         if media_id:
-            log.debug("Found media ID: {0}", media_id)
+            self.logger.debug("Found media ID: {0}", media_id)
             return self._get_streams_from_media(media_id)
 
 
 __plugin__ = DailyMotion
+
 
