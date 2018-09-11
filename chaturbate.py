3bc7bd5cee77f78b91f9d748e90ba19aaecd110e
Damianonymous <zetixpe32@gmail.com>
2018-09-11 09:45:19 +0200

    Update chaturbate.py

--- chaturbate.py
+++ chaturbate.py
@@ -1,14 +1,11 @@
-import re
+import logging
 import uuid
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import http
 from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 
 API_HLS = "https://chaturbate.com/get_edge_hls_url_ajax/"
-
-_url_re = re.compile(r"https?://(\w+\.)?chaturbate\.com/(?P<username>\w+)")
 
 _post_schema = validate.Schema(
     {
@@ -18,14 +15,14 @@ _post_schema = validate.Schema(
     }
 )
 
+log = logging.getLogger(__name__)
+
 
 class Chaturbate(Plugin):
-    @classmethod
-    def can_handle_url(cls, url):
-        return _url_re.match(url)
+    pattern = r'https?://(\w+\.)?chaturbate\.com/(?P<username>\w+)'
 
     def _get_streams(self):
-        match = _url_re.match(self.url)
+        match = self.pattern_re.match(self.url)
         username = match.group("username")
 
         CSRFToken = str(uuid.uuid4().hex.upper()[0:32])
@@ -43,13 +40,14 @@ class Chaturbate(Plugin):
 
         post_data = "room_slug={0}&bandwidth=high".format(username)
 
-        res = http.post(API_HLS, headers=headers, cookies=cookies, data=post_data)
-        data = http.json(res, schema=_post_schema)
+        res = self.session.http.post(API_HLS, headers=headers, cookies=cookies, data=post_data)
+        data = self.session.http.json(res, schema=_post_schema)
 
-        self.logger.info("Stream status: {0}".format(data["room_status"]))
+        log.info("Stream status: {0}".format(data["room_status"]))
         if (data["success"] is True and data["room_status"] == "public" and data["url"]):
             for s in HLSStream.parse_variant_playlist(self.session, data["url"]).items():
                 yield s
 
 
 __plugin__ = Chaturbate
+
